463a7eddb4491e0caf6dc4f314aca3c5a26baa9c
zhilong mu <laomu1988@qq.com>
2016-08-05 14:40:37 +0800

    add-to-update

--- commit.sh
+++ commit.sh
@@ -2,7 +2,13 @@ git add .
 git commit -m 'add-to-update'
 rm -rf _books
 mv books _books
+cp index.html _books/index.html
+cp -f server _books/server
+cp package.json _books/package.json
 git checkout gh-pages
+mv -f _books/index.html index.html
+mv -f _books/server.js server.js
+mv -f _book/server server
 rm -rf books
 mv _books books
 git add .
