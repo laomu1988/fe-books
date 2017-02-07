7ca74ce45a64c51637b521a326432455a6faeddc
muzhilong <laomu1988@qq.com>
2017-02-07 19:20:36 +0800

    简化打包过程

--- build.sh
+++ build.sh
@@ -2,14 +2,25 @@
 folders=("assist" "es6" "fis3" "koa" "node" "react" "tools" "vue" "vue-router")
 output='books'
 
+cd $output
+git checkout gh-pages
+cd ../
+
 if [ "$1"V = "V" ]
 then
-    rm -rf README.md
-    cp assist/README.md README.md
+    rm -rf assist/README.md
+    cp README.md assist/README.md
     for folder in "${folders[@]}"
     do
         sh build.sh "$folder"
     done
+
+    cp -r index.html $output/index.html
+    cp -r server $output/server
+    cp -r server.js $output/server.js
+    cp -r package.json $output/package.json
+    rm -rf assist/README.md
+
 else
     echo "gitbook build $1 $output/$1"
     rm -rf $1/book.json
