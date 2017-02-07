d1bb65c4dd53287e898ae4799cce56b0d3c54001
muzhilong <laomu1988@qq.com>
2017-02-07 19:18:12 +0800

    update

---
+++ server/server/index.js
@@ -0,0 +1,17 @@
+(function () {
+    document.addEventListener('DOMContentLoaded', function () {
+        gitbook.events.on('page.change', function () {
+            var hs = document.querySelector('.page-wrapper').querySelectorAll('h1[id],h2[id],h3[id],h4[id]');
+            var str = '';
+            for (var i = 0; i < hs.length; i++) {
+                var h = hs[i];
+                str += '<li><a href="#' + h.id + '" class="' + h.nodeName.toLowerCase() + '">' + h.textContent + '</a></li>'
+            }
+            if (str) {
+                $('.menu-last').html('<a href="javascript:void(0)">目录</a><ul class="sub">' + str + '</ul>');
+            } else {
+                $('.menu-last').html('');
+            }
+        });
+    }, false);
+})();
