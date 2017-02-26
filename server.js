0f63d3c0cd28977a30eda87b37afab8fcf8a435b
muzhilong <laomu1988@qq.com>
2017-02-26 14:00:25 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = new express();
 
-app.use(express.static(__dirname + ''));
-app.use('/fe-books/', express.static(__dirname));
+app.use(express.static(__dirname + '/books'));
+app.use('/fe-books/', express.static(__dirname + '/books/'));
 
-app.listen(4000, function (err) {
+app.listen(4000, function(err) {
     if (!err) {
         console.log('Start Server At   http://localhost:4000/');
     } else {
