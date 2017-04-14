a07b8dca1eac8d61b0231936456728159f66a854
muzhilong <laomu1988@qq.com>
2017-04-14 13:31:18 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
+        // 创建目录
         gitbook.events.on('page.change', function () {
             var hs = document.querySelector('.page-wrapper').querySelectorAll('h1[id],h2[id],h3[id],h4[id]');
-            var str = '';
+            var str = ''; 
             for (var i = 0; i < hs.length; i++) {
                 var h = hs[i];
                 str += '<li><a href="#' + h.id + '" class="' + h.nodeName.toLowerCase() + '">' + h.textContent + '</a></li>'
